c52fb66008a97c89741e1940cdd32b9ddec656a3
Sri Sethu Madhavan S <124687386+sri-sethu@users.noreply.github.com>
2025-05-31 00:44:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,6 @@ def upload_image():
     return render_template('upload.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
